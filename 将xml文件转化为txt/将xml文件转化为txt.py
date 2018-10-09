1d8da5a322147c5ec7819590f161d903c828ca78
ZHANGJIE <jiezhang_china@163.com>
2018-10-09 09:13:22 +0800

    Add files via upload

---
+++ 将xml文件转化为txt/将xml文件转化为txt.py
@@ -0,0 +1,30 @@
+import xml.etree.ElementTree as ET
+import os
+
+list_file = open('train.txt', 'w')  # 存放的文件
+for filename in os.listdir('./'):  # 遍历xml文件夹
+	if filename.endswith('.xml'):  # 判断后缀是否是.xml
+		in_file = open(filename)  # 打开.xml文件夹
+		tree=ET.parse(in_file)  # 解析.xml文件
+		root = tree.getroot()  # 获取root最大的标签
+		for obj in root.iter('object'):  # 遍历root标签下的object子标签
+
+			clsss = obj.find('name').text  # 获取类别属性，并且给每个类别属性编号
+			if clsss=='container':
+				clsss=0
+			if clsss=='cruise ship':
+				clsss=1
+			if clsss=='drilling platform':
+				clsss=2
+			if clsss=='Lighthouse':
+				clsss=3
+			if clsss=='Sailboat':
+				clsss=4
+			if clsss=='yacht':
+				clsss=5
+
+			xmlbox = obj.find('bndbox')  # 寻找每个object下面的bndbox标签，也就是四个坐标值
+
+			b = xmlbox.find('xmin').text + ',' + xmlbox.find('ymin').text + ',' + xmlbox.find('xmax').text + ',' + xmlbox.find('ymax').text + ',' + str(clsss) + '\n'
+			list_file.write('./'+filename.replace('.xml','.jpg')+' '+b)
+list_file.close()
