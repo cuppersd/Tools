fa0637d0d95117da5d039402a77eb5699554101c
LICHUAN <jiezhang_china@163.com>
2019-01-23 21:51:48 -0500

    Add files via upload

---
+++ C++/给你一个vector和target，找他的两个因数.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+#include<vector>
+#include<unordered_map>
+using namespace std;
+class solution
+{
+public:
+	vector<int> twoSum(vector<int>& nums, int target) {
+		unordered_map<int, int> m;
+		vector<int> res;
+		for (int i = 0; i < nums.size(); i++)
+		{
+			m[nums[i]] = i;
+		}
+
+		
+		for (int i = 0; i < nums.size(); i++)
+		{
+			int t = target - nums[i];
+			if (m.count(t) != 0 && t!=nums[i]) {
+				res.push_back(i);
+				res.push_back(m[t]);
+			}
+			else {
+				continue;
+			}
+		}
+		return res;
+	}
+
+};
+
+int main() {
+	vector<int> list = { 1,8,2,7,11,8,15 };
+	solution ss;
+	vector<int> xx = ss.twoSum(list, 9);
+	for (int i = 0; i < xx.size(); i++) {
+		cout << xx[i] << endl;
+		if (i % 2 == 1) {
+			cout << "--------------------------------" << endl;
+		}
+	}
+	cin.get();
+	return 0;
+}
